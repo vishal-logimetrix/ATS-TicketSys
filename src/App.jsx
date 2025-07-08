36e940094163accd9e1e11cbd42a4e2d238c259d
vishal <vishallengare61@gmail.com>
2025-07-08 11:16:25 +0530

    change the token condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,22 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import DashboardLayout from './Layout/DashboardLayout';
 import NewTicket from './Pages/NewTicket';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Dashboard from './Pages/Dashboard';
-import "./App.css"
+import './App.css';
 import NewUser from './Pages/NewUser';
 import TicketDetailsModal from './Pages/TicketDetails';
 import TicketHistory from './Pages/TicketHistory';
-import { useEffect, useState } from 'react';
 
 const App = () => {
-  // const [token, setToken] = useState(null);
-
-  //   useEffect(() => {
-  //   const savedToken = localStorage.getItem('token');
-  //   setToken(savedToken);
-  // }, []);
-
-  //   if (token === null) {
-  //     console.log("Hello")
-  //   return null; 
-  // }
-  
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route
-        path="/dashboard/*"
-        element={token ? <DashboardLayout /> : <Navigate to="/" />}
-      >
+      <Route path="/dashboard/*" element={<DashboardLayout />}>
         <Route index element={<Dashboard />} />
-        {/* <Route path="tickets" element={<Ticket />} /> */}
         <Route path="tickets" element={<NewTicket />} />
         <Route path="new-user" element={<NewUser />} />
         <Route path="ticket/:id" element={<TicketDetailsModal />} />
